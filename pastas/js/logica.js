86dd899b3ccc442774221017ae35e631802520b3
vitinhohoho <vitorespindola2011@hotmail.com>
2024-11-07 16:19:38 -0300

    Exclusão do prime video e inclusão do javascript

---
+++ pastas/js/logica.js
@@ -0,0 +1,59 @@
+
+// // tipos de dados
+
+// // texto =string
+// const texto = 'Eu sou um texto'
+
+// // numerico = in /float /double
+// const numero_inteiro = 30
+// const numero_flutuante = 18.5
+
+// // logica = boolean
+//     const ligado = false
+
+//     // nulo = null
+//     const presidente = null
+
+//     // Lista de dados = array
+//     const frutas = ['maça', 'goiaba', 'uva']
+    
+//     // não é um numero = NaN (Not a number)
+//     // indefinido = undefined
+
+//     // console.log(typeof(texto))
+//     // console.log(typeof(numero_inteiro))
+//     // console.log(typeof(numero_flutuante))
+//     // console.log(typeof(ligado))
+//     // console.log(typeof(presidente))
+//     // console.log(typeof(frutas))
+
+//     // let idade = parseInt(prompt('informa a sua idade'))
+
+//     // console.log(idade)
+//     // console.log(typeof(idade))
+        
+//     // console.log(parseInt(idade))
+//     // console.log(typeof(parseInt(idade)))
+
+//     let numero1 = parseInt(prompt('primeiro numero')) 
+//     let numero2 = parseInt(prompt('segundo numero')) 
+
+//     let soma = numero1 + numero2
+//     let subtracao = numero1 - numero2
+//     let multiplicacao = numero1 * numero2
+//     let divisao = numero1 / numero2
+
+//     console.log(soma)
+//     console.log(subtracao)
+//     console.log(multiplicacao)
+//     console.log(divisao)
+
+var idade = parseInt(prompt('digite a idade'))
+
+if (idade >= 18){
+    console.log('você é maior de idade')
+}
+else{
+    console.log('menor de idade')
+}
+
